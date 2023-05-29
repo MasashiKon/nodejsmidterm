4158ed711633fca3ba69f9b5447205727c0d8157
MasahiKon <konnonorth@gmail.com>
2023-05-29 03:08:45 -0700

    dupe change public to assets

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('assets'));
 
 app.get("/", (req, res) => {
    res.sendFile(path.join(__dirname, "/index.html"))
