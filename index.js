189100b11af5d0595d56529c8aef19c5825a8600
MasahiKon <konnonorth@gmail.com>
2023-05-29 03:10:25 -0700

    add both static ;

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('assets'));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
    res.sendFile(path.join(__dirname, "/index.html"))
